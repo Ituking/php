fecfdf2c1394edbfdc01285d2725494c502176aa
Ituking <77528963+Ituking@users.noreply.github.com>
2021-08-04 19:15:08 +0900

    演算子の提出
    演算子の提出

---
+++ 2-1/index.php
@@ -0,0 +1,25 @@
+<?php
+    echo 4 + 6;
+    echo "<br>";
+    echo 7 - 3;
+    echo "<br>";
+    echo 3 * 6;
+    echo "<br>";
+    echo 10 / 3;
+    echo "<br>";
+    echo 8 % 5;
+    echo "<br>";
+    $hello = "Hello";
+    $world = "World";
+    echo $hello.$world;
+    echo "<br>";
+    $x++;
+    echo $x;
+    echo "<br>";
+    $x += 6;
+    echo $x;
+?> 
+
+
+
+    
