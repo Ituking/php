d828ec15e9ee69eadd8a23e2281d34cb11803199
Ituking <77528963+Ituking@users.noreply.github.com>
2021-08-11 17:07:14 +0900

    課題提出テスト
    2-12の提出

--- 2-12/index.php
+++ 2-12/index.php
@@ -8,16 +8,13 @@
         <input type="radio" name="product" value="C賞" />C賞<br>
         個数：
         <select name="number">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
+            <?php 
+                for ($i = 1; $i < 11; $i++) { ?>
+                    <option value="<?php echo $i; ?>">
+                        <?php echo $i; ?>
+                    </option>
+                <?php }  
+            ?>
         </select><br>
         <input type="submit" value="申込" />
     </form>
