607fe003a1228bab7c4bd46763e72d22aef26459
Ituking <77528963+Ituking@users.noreply.github.com>
2021-08-18 13:05:31 +0900

    課題提出テスト
    2-17の作成

--- 2-17.1/index.php
+++ 2-17.1/index.php
@@ -1,12 +1,17 @@
 <?php
-    $x = 0;
-    $i = 0;
-    while ($x < 40) {
-        $i++;
-        $s = mt_rand(1, 6);
-        echo $i. "回目 = ". $s;
+    ini_set("display_errors", 1);
+    error_reporting(E_ALL);
+    $total = 0;
+    $kaisu = 1;
+    while ($total <= 40) {
+        // サイコロを用意する
+        $dice = mt_land(1, 6);
+        // 40マス以内ですごろくを終了できるようにする
+        $total += $dice;
+        // 何回目かという表示をするための変数を準備
+        $kaisu++;
+        echo $kaisu."回目 =".$dice;
         echo "<br>";
-        $x += $s;
     }
-    echo "合計". $i. "回目でゴールしました";
+    echo "合計".$total."回目でゴールしました";
 ?>
