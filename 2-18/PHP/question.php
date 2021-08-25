a94eb400ec0d34ad7d3770fba0615d6f1840ca7a
Ituking <77528963+Ituking@users.noreply.github.com>
2021-08-25 16:31:13 +0900

    課題提出テスト
    2-18の作成

--- 2-18/PHP/question.php
+++ 2-18/PHP/question.php
@@ -8,21 +8,23 @@
 //② ①で作成した、配列から正解の選択肢の変数を作成してください
     $what_number_answer = 80;
     $language_answer = "HTML";
-    $command_anser = "select";
+    $command_answer = "select";
 ?>
 <p>お疲れ様です<!--POST通信で送られてきた名前を出力--><?php echo $my_name ?>さん</p>
 <!--フォームの作成 通信はPOST通信で-->
 <h2>①ネットワークのポート番号は何番？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-<?php 
-    foreach ($what_number as $what_number_answer) {
-        echo $what_number_answer;
-    }
-?>
+<?php foreach($what_number as $what_number_answer): ?>
+    <input type="radio" value="">
+<?php endforeach ?>
 <h2>②Webページを作成するための言語は？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-
+<?php foreach($language as $language_answer): ?>
+    <input type="radio" value="">
+<?php endforeach ?>
 <h2>③MySQLで情報を取得するためのコマンドは？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-
+<?php foreach($command as $command_answer): ?>
+    <input type="radio" value="">
+<?php endforeach ?>
 <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
