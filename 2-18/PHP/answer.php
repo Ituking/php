d204c0f1cd2728af8d240c843def9346155db6ba
Ituking <77528963+Ituking@users.noreply.github.com>
2021-09-24 18:59:48 +0900

    課題提出テスト
    2-18の作成

--- 2-18/PHP/answer.php
+++ 2-18/PHP/answer.php
@@ -2,32 +2,52 @@
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
 $my_name = $_POST["my_name"];
 $selected_answer = $_POST["selected_answer"];
+$port = $_POST["post"];
+$language = $_POST["language"];
+$command = $_POST["command"];
 $problem_correct = $_POST["problem_correct"];
 $what_number_correct = $_POST["what_number_correct"];
 $language_correct = $_POST["language_correct"];
 $command_correct = $_POST["command_correct"];
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
-function result_display($selected_answer, $problem_correct) {
-    if ($selected_answer == $problem_correct) {
+function result_display1($port, $what_number_correct) {
+    if ($port == $what_number_correct) {
         echo "正解！";
     } else {
         echo "残念・・・";
     }
 }
+
+function result_display2($language, $language_correct) {
+    if ($language == $language_correct) {
+        echo "正解！";
+    } else {
+        echo "残念・・・";
+    }
+}
+
+function result_display3($command, $command_correct) {
+    if ($command == $command_correct) {
+        echo "正解！";
+    } else {
+        echo "残念・・・";
+    }
+}
+
 ?>
 <p><!--POST通信で送られてきた名前を表示--><?php echo $_POST["my_name"]; ?>さんの結果は・・・？</p>
 <p>①の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php 
-    result_display($selected_answer, $problem_correct);
+    result_display1("port", "what_number_correct");
 ?>
 <p>②の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php
-    result_display($selected_answer, $problem_correct);
+    result_display2("language", "language_correct");
 ?>
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php
-    result_display($selected_answer, $problem_correct);
+    result_display3("command", "command_correct");
 ?>
