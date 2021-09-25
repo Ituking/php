3fdc89dc3f39bee6ffa0423878cf1ad96319526c
Ituking <77528963+Ituking@users.noreply.github.com>
2021-09-25 22:40:32 +0900

    課題提出テスト
    2-18の作成

--- 2-18/PHP/answer.php
+++ 2-18/PHP/answer.php
@@ -10,44 +10,27 @@ $what_number_correct = $_POST["what_number_correct"];
 $language_correct = $_POST["language_correct"];
 $command_correct = $_POST["command_correct"];
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
-function result_display1($port, $what_number_correct) {
-    if ($port == $what_number_correct) {
+function result($x, $y) {
+    if ($x == $y) {
         echo "正解！";
     } else {
         echo "残念・・・";
     }
 }
-
-function result_display2($language, $language_correct) {
-    if ($language == $language_correct) {
-        echo "正解！";
-    } else {
-        echo "残念・・・";
-    }
-}
-
-function result_display3($command, $command_correct) {
-    if ($command == $command_correct) {
-        echo "正解！";
-    } else {
-        echo "残念・・・";
-    }
-}
-
 ?>
 <p><!--POST通信で送られてきた名前を表示--><?php echo $_POST["my_name"]; ?>さんの結果は・・・？</p>
 <p>①の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php 
-    result_display1("port", "what_number_correct");
+    result($port, $what_number_correct);
 ?>
 <p>②の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php
-    result_display2("language", "language_correct");
+    result($language, $language_correct);
 ?>
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php
-    result_display3("command", "command_correct");
+    result($command, $command_correct);
 ?>
--- 2-18/PHP/question.php
+++ 2-18/PHP/question.php
@@ -31,6 +31,15 @@
 <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
     <button type="submit">回答する</button>
     <input type="hidden" name="what_number_correct" value="<?php echo $_POST["what_number_correct"]; ?>">
+    <?php
+        var_dump($what_number_correct);
+    ?>
     <input type="hidden" name="language_correct" value="<?php echo $_POST["language_correct"]; ?>">
+    <?php
+        var_dump($language_correct);
+    ?>
     <input type="hidden" name="command_correct" value="<?php echo $_POST["command_correct"]; ?>">
+    <?php
+        var_dump($command_correct);
+    ?>
 </form>
