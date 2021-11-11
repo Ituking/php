f8340f3c865571c0bf7c4cb07e68097e24c50250
Ituking <77528963+Ituking@users.noreply.github.com>
2021-11-12 08:09:35 +0900

    課題提出テスト
    3-4の作成

---
+++ 3-4/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <div class="header-left">
+            <img src="1599315827_logo.png">
+        </div>
+        <div class="header-right">
+            <div class="up">
+
+            </div>
+            <div class="bottom">
+
+            </div>
+        </div>
+    </div>
+    <div class="main">
+
+    </div>
+    <div class="footer">
+
+    </div>
+</body>
+</html>
