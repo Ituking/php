51dad31995a7978c820881664e14049873d243e5
Ituking <77528963+Ituking@users.noreply.github.com>
2021-12-23 16:12:05 +0900

    課題提出テスト
    3-4の作成

--- 3-4/index.php
+++ 3-4/index.php
@@ -1,3 +1,13 @@
+<?php 
+    require_once("pdo.php");
+    require_once("getData.php");
+
+    $category = ["category_no" => ["1" => "食事", "2" => "旅行", "3" => "その他"]];
+    
+    $a = new getData();
+    $b = $a->getPostData();
+    
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -13,111 +23,45 @@
             <img class="header-left-image" src="image/logo.png">
         </div>
         <div class="header-right">
-            <div class="header-right-top">
-                <p><?php
-                    $db = new mysqli("localhost:3306", "root", "root", "checktest4");
-                    $users = $db->query("select * from users");
-                    if ($users) {
-                        while ($user = $users->fetch_assoc()) {
-                            echo "ようこそ ". $user["last_name"]. $user["first_name"]. " さん";
-                        }
-                    } else {
-                        echo $db->error;
-                    }
-                ?></p>
-                         
-            </div>
-            <div class="header-right-bottom">
-                <p><?php 
-                    $db = new mysqli("localhost:3306", "root", "root", "checktest4");
-                    $users = $db->query("select * from users");
-                    if ($users) {
-                        while ($user = $users->fetch_assoc()) {
-                            echo "最終ログイン日 : ". $user["last_login"];
-                        }
-                    } else {
-                        echo $db->error;
-                    }
-                ?></p>
-            </div>
+            <div class="header-right-top"></div>
+            <div class="header-right-bottom"></div>
         </div>
     </div>
     <div class="main">
-        <table class = "main-title" border = "1">
-            <tr>
+        <div class="main-title">
+            <table border="1">
                 <th>記事ID</th>
                 <th>タイトル</th>
                 <th>カテゴリ</th>
                 <th>本文</th>
                 <th>投稿日</th>
-            </tr>  
-        </table>
-        <table class = "main-posts1" border = "1">
-                <?php 
-                    $db = new mysqli("localhost:3306", "root", "root", "checktest4");
-                    $posts = $db->query("select * from posts where id = 5");
-                    if ($posts) {
-                        while ($post = $posts->fetch_assoc()) {
-                            echo "<tr><td>{$post["id"]}</td><td>{$post["title"]}</td><td>{$post["category"]}</td><td>{$post["comment"]}</td><td>{$post["created"]}</td></tr>";
-                        }
-                    } else {
-                        echo $db->error;
-                    }
-            ?>
-        </table>
-        <table class = "main-posts2" border = "1">
-                <?php 
-                    $db = new mysqli("localhost:3306", "root", "root", "checktest4");
-                    $posts = $db->query("select * from posts where id = 4");
-                    if ($posts) {
-                        while ($post = $posts->fetch_assoc()) {
-                            echo "<tr><td>{$post["id"]}</td><td>{$post["title"]}</td><td>{$post["category"]}</td><td>{$post["comment"]}</td><td>{$post["created"]}</td></tr>";
-                        }
-                    } else {
-                        echo $db->error;
-                    }
-            ?>    
-        </table>
-        <table class = "main-posts3" border = "1">
-                <?php 
-                    $db = new mysqli("localhost:3306", "root", "root", "checktest4");
-                    $posts = $db->query("select * from posts where id = 3");
-                    if ($posts) {
-                        while ($post = $posts->fetch_assoc()) {
-                            echo "<tr><td>{$post["id"]}</td><td>{$post["title"]}</td><td>{$post["category"]}</td><td>{$post["comment"]}</td><td>{$post["created"]}</td></tr>";
-                        }
-                    } else {
-                        echo $db->error;
-                    }
-            ?>    
-        </table>
-        <table class = "main-posts4" border = "1">
-                <?php 
-                    $db = new mysqli("localhost:3306", "root", "root", "checktest4");
-                    $posts = $db->query("select * from posts where id = 2");
-                    if ($posts) {
-                        while ($post = $posts->fetch_assoc()) {
-                            echo "<tr><td>{$post["id"]}</td><td>{$post["title"]}</td><td>{$post["category"]}</td><td>{$post["comment"]}</td><td>{$post["created"]}</td></tr>";
-                        }
-                    } else {
-                        echo $db->error;
-                    }
-            ?>    
-        </table>
-        <table class = "main-posts5" border = "1">
-                <?php 
-                    $db = new mysqli("localhost:3306", "root", "root", "checktest4");
-                    $posts = $db->query("select * from posts where id = 1");
-                    if ($posts) {
-                        while ($post = $posts->fetch_assoc()) {
-                            echo "<tr><td>{$post["id"]}</td><td>{$post["title"]}</td><td>{$post["category"]}</td><td>{$post["comment"]}</td><td>{$post["created"]}</td></tr>";
-                        }
-                    } else {
-                        echo $db->error;
-                    }
-            ?>    
-        </table>
-    </div>  
+            </table>
+        </div>
+        <div class="main-posts">
+            <table border="1">
+                    <?php 
+                        foreach ($b as $c) { ?>
+                          <tr>
+                            <td><?php echo $c["id"] ?></td>
+                            <td><?php echo $c["title"] ?></td>
+                            <td><?php 
+                                if ($c["category_no"] == 1) {
+                                    echo "食事";
+                                } elseif ($c["category_no"] == 2) {
+                                    echo "旅行";
+                                } else {
+                                    echo "その他";
+                                } ?>
+                            </td>
+                            <td><?php echo $c["comment"] ?></td>
+                            <td><?php echo $c["created"] ?></td>
+                        </tr>
+                    <?php    } ?> 
+            </table>
+        </div>   
+    </div>
+    <div class="footer">
+        <p>Y&I group.inc</p>                            
+    </div>
 </body>
-<div class="footer"><a>Y&I group.inc</a></div>
 </html>
