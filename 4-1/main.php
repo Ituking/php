b7780271491e0e79b4645f8f959fc4cdf9aeb3ca
Ituking <77528963+Ituking@users.noreply.github.com>
2022-01-14 01:40:16 +0900

    課題提出テスト
    4-1の作成

--- 4-1/main.php
+++ 4-1/main.php
@@ -1,5 +1,3 @@
 <?php 
-    $userName = $_POST["name"];
-    $userPassword = $_POST["password"];
-    $userData = $_POST["signUp"];
+    
 ?>
--- 4-1/signUp.php
+++ 4-1/signUp.php
@@ -4,50 +4,37 @@ require ("db_connect.php");
 
 // POSTで送られていれば処理実行
 if (isset($_POST["signUp"])) {
+    // PDOのインスタンスを取得FILL_IN
+    $pdh = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+    try {
+        // SQL文の準備 FILL_IN
+        $sql = "insert into users (name, password) values ($userName, $userPassword)"; 
+        // パスワードをハッシュ化
+        $userPassword = $_POST["password"];
+        $password_hash = password_hash($userPassword, PASSWORD_DEFAULT);
+        // プリペアドステートメントの作成 FILL_IN
+        $stmt = $pdo->prepare($sql);
+        // 値をセット FILL_IN
+        $stmt->bindParam(":name", $userName);
+        $stmt->bindParam(":password", $password_hash);
+        // 実行 FILL_IN
+        $stmt->execute();
+        // 登録完了メッセージ出力
+        echo "登録が完了しました。";
+    }catch (PDOException $e) {
+        // エラーメッセージの出力 FILL_IN
+        echo "登録が完了していません。";
+        // 終了 FILL_IN
+        die();
+    }
     // nameとpassword両方送られてきたら処理実行
-    if (!isset($_POST["name"]) && !isset($_POST["password"])) {
+    if (isset($_POST["name"]) && isset($_POST["password"])) {
         echo "登録が完了しました。";
     } else {
         echo "登録が完了していません。";
     }
 }
 
-function db_connect() {
-    try {
-        // PDOインスタンスの作成
-        $pdo = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
-        // エラー処理方法の設定
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch(PDOException $e) {
-        echo 'Error: ' . $e->getMessage();
-        die();
-    }
-}
-
-$pdo = db_connect();
-// PDOのインスタンスを取得FILL_IN
-$pdh = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
-try {
-    // SQL文の準備 FILL_IN
-    $sql = "insert into users (name, password) values ($userName, $userPassword)"; 
-    // パスワードをハッシュ化
-    $password_hash = password_hash($userPassword, PASSWORD_DEFAULT);
-    // プリペアドステートメントの作成 FILL_IN
-    $stmt = $pdo->prepare($sql);
-    // 値をセット FILL_IN
-    $stmt->bindParam(":name", $userName);
-    $stmt->bindParam(":password", $userPassword);
-    // 実行 FILL_IN
-    $stmt->execute();
-    // 登録完了メッセージ出力
-    echo "登録が完了しました。";
-}catch (PDOException $e) {
-    // エラーメッセージの出力 FILL_IN
-    echo "登録が完了していません。";
-    // 終了 FILL_IN
-    die();
-}
 ?>
 
 <!DOCTYPE html>
