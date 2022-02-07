02b705852855365db0eb479c3e4863420fa0ec3e
Ituking <77528963+Ituking@users.noreply.github.com>
2022-02-07 23:18:40 +0900

    課題提出テスト
    4-1の作成

--- 4-1/db_connect.php
+++ 4-1/db_connect.php
@@ -1,5 +1,5 @@
 <?php 
-    define('DB_DATABASE', 'yigroupBlog');
+    define('DB_DATABASE', 'YIgroupBlog');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', 'root');
     define('PDO_DSN', 'mysql:host=localhost;charset=utf8;dbname='.DB_DATABASE);
