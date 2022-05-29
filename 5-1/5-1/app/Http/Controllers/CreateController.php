98bc22068c83c9251c6e9b35e42928fb74729d42
Ituking <77528963+Ituking@users.noreply.github.com>
2022-05-29 23:13:05 +0900

    課題提出テスト
    5-1の作成

--- 5-1/5-1/app/Http/Controllers/CreateController.php
+++ 5-1/5-1/app/Http/Controllers/CreateController.php
@@ -4,13 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Post;
+
 class CreateController extends Controller
 {
     public function add() {
         return view('create');
     }
 
-    public function create() {
+    public function create(Request $request) {
+
+        $this->validate($request, Post::$rules);
+
+        $posts = new Post;
+        $form = $request->all();
+
+        if (isset($form['video'])) {
+            $path = $request->video('video')->store('public/video');
+            $posts->video_path = basename($path);
+        } else {
+            $posts->video_path = null;
+        }
+
+        unset($form['_token']);
+        unset($form['video']);
+
+        $posts->fill($form);
+        $posts->save();
+        
         return view('create');
     }
 
